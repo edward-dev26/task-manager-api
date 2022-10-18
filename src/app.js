fdbf95fc4b139e1faf455152781b9a39a941487d
Eduard Shvetsov <eshvetsov@griddynamics.com>
2022-10-18 09:31:41 +0300

    Add index route

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const tasksRouter = require('./routers/tasks');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to TaskManagerAPI</h1>")
+});
+
 app.use(express.json());
 app.use(usersRouter);
 app.use(tasksRouter)
